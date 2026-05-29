0d79cf9b7705a6faec1a91f4b81626df5ea1c1c5
Pamella Pereto <pamellapereto@gmail.com>
2026-05-29 19:42:18 -0300

    feature: armazenar dados via localStorage

--- js/app.js
+++ js/app.js
@@ -1,14 +1,34 @@
+const form = document.getElementById("form");
+
 const inputNome = document.getElementById("nome");
 const inputEmail = document.getElementById("email");
 const inputMedicamento = document.getElementById("medicamento");
 const inputData = document.getElementById("data");
 const inputQtd = document.getElementById("qtd-dosagem");
+const dosagem = document.querySelectorAll('input[name=dosagem]');
+/*dosagem = ["250",     "500",        "750",     "1000"]; 
+            i = 0       i = 1         i = 2       i = 3
+           início       intervalo                  fim              */
+
+//1ª forma para percorrer a lista
+for (i = 0; i < dosagem.lenght();  i++)  {
+    console.log(dosagem[i]);
+}  
+          
+
+
+const btnReset = document.getElementById("reset");
+const btnSalvar = document.createElement("button");
+btnSalvar.textContent = "Salvar";
+form.appendChild(btnSalvar);
+
 
 const keyNome = "mh-nome";
 const keyEmail = "mh-email";
 const keyMedicamento = "mh-medicamento";
 const keyData = "mh-data";
 const keyQtd = "mh-qtd";
+const keyDosagem = "mh-dosagem";
 
 function salvarNome() {
   localStorage.setItem("mh-nome", inputNome.value);
@@ -29,11 +49,18 @@ function salvarQtd() {
   localStorage.setItem("mh-qtd", inputQtd.value);
 }
 
+function salvarOpDosagem() {
+  localStorage.setItem("mh-dosagem", opDosagem.value);
+}
+
 inputNome.addEventListener("input", salvarNome);
 inputEmail.addEventListener("input", salvarEmail);
 inputMedicamento.addEventListener("input", salvarMedicamento);
 inputData.addEventListener("change", salvarData);
 inputQtd.addEventListener("change", salvarQtd);
+btnReset.addEventListener("click", resetar);
+
+
 
 function carregarDados() {
   const nomeSalvo = localStorage.getItem("mh-nome");
